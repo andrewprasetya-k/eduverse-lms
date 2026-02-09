e396e4ac7b39fca9ee1a584bd1a418f40b8e8005
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-02-09 10:28:53 +0700

    refactor: added school repo

---
+++ backend/internal/repository/school_repo.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"backend/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type SchoolRepository interface{
+	CreateSchool(school *domain.School) error
+	GetAllSchools() ([]*domain.School, error)
+	GetSchoolByID(id string) (*domain.School, error)
+	UpdateSchool(school *domain.School) error
+	DeleteSchool(id string) error
+}
+
+type schoolRepository struct {
+	db *gorm.DB
+}
+
+//constructor
+func NewSchoolRepository(db *gorm.DB) SchoolRepository {
+	return &schoolRepository{db: db}
+}
+
+func (r *schoolRepository) CreateSchool(school *domain.School) error {
+	return r.db.Create(school).Error
+}
+
+func (r *schoolRepository) GetAllSchools() ([]*domain.School, error) {
+	var schools []*domain.School
+	err := r.db.Find(&schools).Error
+	return schools, err
+}
+
+func (r *schoolRepository) GetSchoolByID(id string) (*domain.School, error) {
+	var school domain.School
+	err := r.db.First(&school, "sch_id = ?", id).Error
+	return &school, err
+}
+
+func (r *schoolRepository) UpdateSchool(school *domain.School) error {
+	return r.db.Save(school).Error
+}
+
+func (r *schoolRepository) DeleteSchool(id string) error {
+	return r.db.Delete(&domain.School{}, "sch_id = ?", id).Error
+}
