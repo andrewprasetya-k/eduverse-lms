cedd48a4e06c23e8cc2ac72f2d27b61e9da104bd
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-02-09 11:23:20 +0700

    refactor: added school handler

---
+++ backend/internal/handler/school_handler.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"backend/internal/domain"
+	"backend/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SchoolHandler struct {
+	service service.SchoolService
+}
+
+func NewSchoolHandler(service service.SchoolService) *SchoolHandler {
+	return &SchoolHandler{service: service}
+}
+
+//Post
+func (h *SchoolHandler) CreateSchool(c *gin.Context) {
+	var school domain.School
+	//parse json dari request body ke struct school
+	if err:= c.ShouldBindJSON(&school); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	//panggil service untuk create school
+	if err := h.service.CreateSchool(&school); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	//return response
+	c.JSON(http.StatusCreated, school)
+}
+
+//Get All
+func (h *SchoolHandler) GetAllSchools(c *gin.Context) {
+	schools, err := h.service.GetAllSchools()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, schools)
+}
+
+//Get By ID
+func (h *SchoolHandler) GetSchoolByID(c *gin.Context) {
+	id := c.Param("id")
+	school, err := h.service.GetSchoolByID(id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, school)
+}
+
+//Put
+func (h *SchoolHandler) UpdateSchool(c *gin.Context) {
+	var school domain.School
+	//parse json dari request body ke struct school
+	if err:= c.ShouldBindJSON(&school); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	//panggil service untuk update school
+	if err := h.service.UpdateSchool(&school); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	//return response
+	c.JSON(http.StatusOK, school)
+}
+
+//Delete
+func (h *SchoolHandler) DeleteSchool(c *gin.Context) {
+	id := c.Param("id")
+	if err := h.service.DeleteSchool(id); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "School deleted successfully"})
+}
