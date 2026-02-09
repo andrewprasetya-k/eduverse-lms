dcfe67a5b982f22d0c9571ec73a7ba70dd3bc643
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-02-09 15:12:30 +0700

    feat(school code): add existing code security checking

--- backend/internal/service/school_service.go
+++ backend/internal/service/school_service.go
@@ -3,11 +3,15 @@ package service
 import (
 	"backend/internal/domain"
 	"backend/internal/repository"
+	"errors"
+	"fmt"
 	"math/rand"
 	"time"
+
+	"gorm.io/gorm"
 )
 
-type SchoolService interface{
+type SchoolService interface {
 	CreateSchool(school *domain.School) error
 	GetAllSchools() ([]*domain.School, error)
 	GetSchoolByID(id string) (*domain.School, error)
@@ -20,27 +24,52 @@ type schoolService struct {
 	repo repository.SchoolRepository
 }
 
-//constructor
+// constructor
 func NewSchoolService(repo repository.SchoolRepository) SchoolService {
 	return &schoolService{repo: repo}
 }
 
 func (s *schoolService) CreateSchool(school *domain.School) error {
+	// 1. Jika code kosong, generate otomatis dengan pengecekan keunikan
 	if school.Code == "" {
-		word := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-		code := make([]rune, 6)
-		seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
-		for i := range code {
-			code[i] = word[seededRand.Intn(len(word))]
+		for {
+			newCode := s.generateRandomCode()
+			codeExist, err := s.repo.GetSchoolByCode(newCode)
+			if codeExist != nil {
+				if errors.Is(err, gorm.ErrRecordNotFound) {
+					school.Code = newCode
+					break
+				}
+				return err
+			}
 		}
-		school.Code = string(code)
+	} else {
+		codeExist, err := s.repo.GetSchoolByCode(school.Code)
+		if codeExist != nil {
+			return fmt.Errorf("school code '%s' already exists", school.Code)
+		}
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return err
+		}
 	}
+
 	return s.repo.CreateSchool(school)
+}
+
+func (s *schoolService) generateRandomCode() string {
+	word := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	code := make([]rune, 6)
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := range code {
+		code[i] = word[seededRand.Intn(len(word))]
+	}
+	return string(code)
 }
 
 func (s *schoolService) GetAllSchools() ([]*domain.School, error) {
 	return s.repo.GetAllSchools()
 }
+
 
 func (s *schoolService) GetSchoolByID(id string) (*domain.School, error) {
 	return s.repo.GetSchoolByID(id)
