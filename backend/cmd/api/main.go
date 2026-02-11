45857db695097de42bb927a2714dd749466f90b9
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-02-11 11:53:59 +0700

    hard reset, bahasa aneh, gajelas lah anjegnggg

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"backend/internal/handler"
-	"backend/internal/repository"
-	"backend/internal/service"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -21,22 +16,15 @@ func main() {
 	}
 
 	//db connection
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  dsn,
-		PreferSimpleProtocol: true, // Mengatasi error prepared statement pada Supabase Pooler
-	}), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database: " + err.Error())
-	}
+	// db, err := gorm.Open(postgres.New(postgres.Config{
+	// 	DSN:                  dsn,
+	// 	PreferSimpleProtocol: true, // Mengatasi error prepared statement pada Supabase Pooler
+	// }), &gorm.Config{})
+	// if err != nil {
+	// 	panic("failed to connect database: " + err.Error())
+	// }
 
 	//initialize repo, service, handler
-	schoolRepo := repository.NewSchoolRepository(db)
-	schoolService := service.NewSchoolService(schoolRepo)
-	schoolHandler := handler.NewSchoolHandler(schoolService)
-
-	subjectRepo := repository.NewSubjectRepository(db)
-	subjectService := service.NewSubjectService(subjectRepo, schoolRepo)
-	subjectHandler := handler.NewSubjectHandler(subjectService)
 
 	//router setup
 	r := gin.Default()
@@ -47,29 +35,10 @@ func main() {
 		})
 	})
 
-	api := r.Group("/api")
-	{
-		// School Routes
-		schools := api.Group("/schools")
-		{
-			schools.POST("", schoolHandler.CreateSchool)
-			schools.GET("", schoolHandler.GetAllSchools)
-			// schools.GET("/:id", schoolHandler.GetSchoolByID)
-			schools.GET("/:schoolCode", schoolHandler.GetSchoolByCode)
-			schools.PATCH("/:schoolCode", schoolHandler.UpdateSchool)
-			schools.DELETE("/:schoolCode", schoolHandler.DeleteSchool)
-		}
-
-		// Subject Routes
-		subjects := api.Group("/subjects")
-		{
-			subjects.POST("", subjectHandler.CreateSubject)
-			subjects.GET("/:schoolCode", subjectHandler.GetAllSubjects)
-			subjects.GET("/:schoolCode/:subjectCode", subjectHandler.GetSubjectByCode)
-			subjects.PATCH("/:subjectCode", subjectHandler.UpdateSubject)
-			subjects.DELETE("/:subjectCode", subjectHandler.DeleteSubject)
-		}
-	}
+	// api := r.Group("/api")
+	// {
+	// 	//todo: routes group
+	// }
 
 	//run server
 	r.Run(":8080")
