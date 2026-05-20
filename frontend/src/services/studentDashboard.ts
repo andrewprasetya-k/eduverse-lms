d987eb1bac6eb78f39dfd2905646e0959f429c04
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-05-20 15:04:05 +0700

    fix(url fetching): change api endpoint fetching after api url modification

--- frontend/src/services/studentDashboard.ts
+++ frontend/src/services/studentDashboard.ts
@@ -17,7 +17,7 @@ export async function getMemberClasses(schoolUserId: string) {
 }
 
 export async function getRecentNotifications() {
-  const { data } = await api.get<NotificationListResponse>('/notifications/', {
+  const { data } = await api.get<NotificationListResponse>('/notifications', {
     params: { page: 1, limit: 5 },
   })
   return data
